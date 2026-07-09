208d7a6e83b2fe7ed7a152443f1ba6e9ae1e071e
robertomattus <robertmatt113@gmail.com>
2026-07-09 13:57:00 -0500

    feat: implement auth and tab navigation structure with sign-in, sign-up, and tab screen views

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,9 @@
+import { Redirect, Slot } from "expo-router";
+
+export default function _Layout() {
+  const isAuthenticated = true;
+
+  if (!isAuthenticated) return <Redirect href="/sign-in" />;
+
+  return <Slot />;
+}
